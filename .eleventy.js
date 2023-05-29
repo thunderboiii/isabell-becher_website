314867a1d677fcda9f46811c7647b70ed87c8733
thunderboiii <emil@maple-ink.com>
2023-05-29 16:06:30 +0200

    Updated Passthrough

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,6 @@
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy('./src/style.css');
+    eleventyConfig.addPassthroughCopy('./src/admin');
 
     eleventyConfig.addPassthroughCopy('./src/img');
     eleventyConfig.addPassthroughCopy('./src/fonts');
