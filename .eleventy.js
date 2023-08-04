3eeedb7c1b01345068dbab080f10ca0b8fdd8341
thunderboiii <emil@maple-ink.com>
2023-08-04 17:33:51 +0200

    stuff

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,11 @@
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy('./src/style.css');
     eleventyConfig.addPassthroughCopy('./src/admin');
-    eleventyConfig.addPassthroughCopy('./src/contactform.php');
 
     eleventyConfig.addPassthroughCopy('./src/img');
     eleventyConfig.addPassthroughCopy('./src/fonts');
     eleventyConfig.addPassthroughCopy('./src/scripts');
+    eleventyConfig.addPassthroughCopy('./src/includes');
     
     return {
         dir: {
